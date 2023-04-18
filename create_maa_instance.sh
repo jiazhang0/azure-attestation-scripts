29f9461c7be5da58f39fefc217602ab446396992
Jia Zhang <zhang.jia@linux.alibaba.com>
2023-04-18 18:12:53 +0800

    create_maa_instance.sh: support to import the initial policy signing certificate
    Signed-off-by: Jia Zhang <zhang.jia@linux.alibaba.com>

--- create_maa_instance.sh
+++ create_maa_instance.sh
@@ -2,7 +2,14 @@
 
 set -e
 
-az attestation create \
-  --name $AZURE_MAA_CUSTOM_RESOURCE_NAME \
-  --resource-group $AZURE_RESOURCE_GROUP \
+cmd_to_run=$(cat <<EOF
+az attestation create
+  --name $AZURE_MAA_CUSTOM_RESOURCE_NAME
+  --resource-group $AZURE_RESOURCE_GROUP
   --location $AZURE_RESOURCE_GROUP_LOCATION
+EOF
+)
+
+[ -f root_policy_signing_cert.pem ] && cmd_to_run="$cmd_to_run --certs-input-path root_policy_signing_cert.pem"
+
+eval $cmd_to_run
