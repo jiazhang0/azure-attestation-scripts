abad5cf38260fe43ae8cf6bb680813a694dad04e
Jia Zhang <zhang.jia@linux.alibaba.com>
2023-04-18 18:10:03 +0800

    configure_policy.sh: fix the generation of policy JWS in AAD trust model
    Signed-off-by: Jia Zhang <zhang.jia@linux.alibaba.com>

--- configure_policy.sh
+++ configure_policy.sh
@@ -13,22 +13,22 @@ if [[ "$maa_trust_model" == "Isolated" ]]; then
   ./sign_jws.py --payload policy \
     --signing-key my_policy_signing_private_key.pem \
     --signing-cert my_policy_signing_cert.pem
-
-  policy_to_configure=$(cat policy.jws)
 elif [[ "$maa_trust_model" == "AAD" ]]; then
-  policy_to_configure=$(cat policy)
+  ./sign_jws.py --payload policy
 else
   echo "Unsupported MAA trust model $maa_trust_model"
   exit 1
 fi
 
+policy_to_configure=policy.jws
+
 if [[ $USE_AZ_CLI == "1" ]]; then
   cmd_to_run=$(cat <<EOF
 az attestation policy set
---name \$AZURE_MAA_CUSTOM_RESOURCE_NAME
---resource-group \$AZURE_RESOURCE_GROUP
---attestation-type \$AZURE_ATTESTATION_TYPE_FOR_AZ_CLI
---new-attestation-policy "\$policy_to_configure"
+  --name \$AZURE_MAA_CUSTOM_RESOURCE_NAME
+  --resource-group \$AZURE_RESOURCE_GROUP
+  --attestation-type \$AZURE_ATTESTATION_TYPE_FOR_AZ_CLI
+  --new-attestation-policy-file "\$policy_to_configure"
 EOF
 )
 
@@ -41,7 +41,7 @@ else
     -H "Authorization: Bearer `az account get-access-token \
       --resource https://attest.azure.net \
       --query accessToken --output tsv`" \
-    -d "$policy_to_configure" \
+    -d "$(cat $policy_to_configure)" \
     "https://$AZURE_MAA_ENDPOINT/policies/$AZURE_ATTESTATION_TYPE?api-version=2022-08-01" | \
   jq .
 fi
