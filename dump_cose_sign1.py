42c932294e6b8bba161a6ca4bf040d843bae49aa
Jia Zhang <zhang.jia@linux.alibaba.com>
2023-04-26 11:12:17 +0800

    Add the sample cose sign1 signature and dump tool
    Signed-off-by: Jia Zhang <zhang.jia@linux.alibaba.com>

---
+++ dump_cose_sign1.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+from cose.messages import CoseMessage
+
+with open("samples/body.uvm_reference_info.bin", "rb") as f:
+    encoded = f.read()
+
+decoded = CoseMessage.decode(encoded)
+print(decoded)
