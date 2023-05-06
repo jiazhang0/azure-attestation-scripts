9d3e77b4f4e9da5c6ab100694a8cb88e1f0c09fa
Jia Zhang <zhang.jia@linux.alibaba.com>
2023-05-06 14:06:50 +0800

    ci_test.sh: call create/delete_maa_instance.sh
    Signed-off-by: Jia Zhang <zhang.jia@linux.alibaba.com>

--- ci_test.sh
+++ ci_test.sh
@@ -6,16 +6,17 @@ source ./env.sh
 
 function cleanup() {
   set +e
-  ./reset_policy.sh
-  ./delete_policy_signing_cert.sh
+  ./delete_maa_instance.sh
 }
 trap cleanup ERR
 
+./create_maa_instance.sh
 ./add_policy_signing_cert.sh
 ./list_policy_signing_cert.sh
 ./configure_policy.sh
 ./show_policy.sh
 ./reset_policy.sh
 ./delete_policy_signing_cert.sh
+./delete_maa_instance.sh
 
 echo "All tests complete!"
