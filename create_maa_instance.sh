ad95255b90c98d181d6c2debb0e8a0a741751bcd
Jia Zhang <zhang.jia@linux.alibaba.com>
2023-05-06 14:05:55 +0800

    create_maa_instance.sh: add the role assignments
    Signed-off-by: Jia Zhang <zhang.jia@linux.alibaba.com>

--- create_maa_instance.sh
+++ create_maa_instance.sh
@@ -14,3 +14,14 @@ EOF
 [[ "$DEBUG" == "1" ]] && cmd_to_run="$cmd_to_run --debug --verbose"
 
 eval $cmd_to_run
+
+# Assign RBAC roles to the resource owner so they can set policy
+scope=`az attestation show -n $AZURE_MAA_CUSTOM_RESOURCE_NAME -g $AZURE_RESOURCE_GROUP --query id -o tsv`
+az role assignment create \
+  --role "Attestation Contributor" \
+  --assignee `az ad user list --query [0].id -o tsv` \
+  --scope $scope
+az role assignment create \
+  --role "Attestation Reader" \
+  --assignee `az ad user list --query [0].id -o tsv` \
+  --scope $scope
